0bf1ee5adc5be43ccd23fb2b24ed08b635836316
gideo <gideonsthawk@gmail.com>
2023-11-20 16:42:46 -0500

    added smelting recipes added crafting recipe for alexandrite and alexandrite block

--- src/main/java/net/gideon/mccourse/block/custom/SoundBlock.java
+++ src/main/java/net/gideon/mccourse/block/custom/SoundBlock.java
@@ -2,16 +2,20 @@ package net.gideon.mccourse.block.custom;
 
 import net.gideon.mccourse.MCCourseMod;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.Nullable;
 
 public class SoundBlock extends Block {
     public SoundBlock(Properties pProperties) {
@@ -26,26 +30,34 @@ public class SoundBlock extends Block {
 
             if (pPlayer.isCrouching()){
 
-                pLevel.playSound(pPlayer ,pPos, SoundEvents.NOTE_BLOCK_BANJO.get(), SoundSource.BLOCKS,1f, 1f );
+                pLevel.playSound(null ,pPos, SoundEvents.NOTE_BLOCK_BANJO.get(), SoundSource.BLOCKS,1f, 1f );
 
                 return InteractionResult.SUCCESS;
 
             } else {
 
-                pLevel.playSound(pPlayer, pPos, SoundEvents.NOTE_BLOCK_COW_BELL.get(), SoundSource.BLOCKS, 1f, 1f);
+                pLevel.playSound(null, pPos, SoundEvents.NOTE_BLOCK_COW_BELL.get(), SoundSource.BLOCKS, 1f, 1f);
 
                 return InteractionResult.CONSUME;
             }
 
         }
 
-
-
-
-
-
-
-
         return super.use(pState, pLevel, pPos, pPlayer, pHand, pHit);
+    }
+
+    @Override
+    public void stepOn(Level pLevel, BlockPos pPos, BlockState pState, Entity pEntity) {
+
+        pLevel.playSound(pEntity,pPos,SoundEvents.NOTE_BLOCK_BIT.get(), SoundSource.BLOCKS, 1f, 1f );
+
+
+        super.stepOn(pLevel, pPos, pState, pEntity);
+    }
+
+    @Override
+    public void onPlace(BlockState pState, Level pLevel, BlockPos pPos, BlockState pOldState, boolean pMovedByPiston) {
+        pLevel.playSound(null,pPos,SoundEvents.NOTE_BLOCK_BIT.get(), SoundSource.BLOCKS, 1f, 1f );
+        super.onPlace(pState, pLevel, pPos, pOldState, pMovedByPiston);
     }
 }
